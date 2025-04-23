2a50ac39cade2910f6ef879fa5ff8dbf27a0e421
InfoTechGold <empresainfotechgold@gmail.com>
2025-04-23 18:22:23 -0300

    atualização do código java gerenciarBanco
    atualização do código java gerenciarBanco

--- gerenciarBanco.java
+++ gerenciarBanco.java
@@ -146,9 +146,9 @@ public class gerenciarBanco {
                 else System.out.println("valor invalido"); 
                 
             }
-              if(opcao==0){
-                   System.out.println("muito obrigado por sua preferência até mais !");
-              }
+            if(opcao==0){
+                System.out.println("muito obrigado por sua preferência até mais !");
+            }
             //caso inserir número invalido mostro na tela valor invalido
                 //usando uma operação ternaria
             else System.out.println("Valor invalido");
